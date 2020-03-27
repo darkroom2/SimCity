4820e5046b6b82f6439703def1ffffb46098e965
radekk <rkomorowski97@gmail.com>
2020-03-27 15:32:40 +0100

    quick level creation

--- Game.cpp
+++ Game.cpp
@@ -8,7 +8,7 @@
 void Game::initVariables() {
     this->window = nullptr;
     this->title = "SimCity 2137";
-    this->scale_factor = 2.0f;
+    this->gui_scale_factor = 2.0f;
 }
 
 void Game::initWindow() {
@@ -16,8 +16,92 @@ void Game::initWindow() {
     this->window->setVerticalSyncEnabled(true);
     ImGui::SFML::Init(*this->window);
 
-    ImGui::GetStyle().ScaleAllSizes(this->scale_factor);
-    ImGui::GetIO().FontGlobalScale = this->scale_factor;
+    ImGui::GetStyle().ScaleAllSizes(this->gui_scale_factor);
+    ImGui::GetIO().FontGlobalScale = this->gui_scale_factor;
+}
+
+void Game::initPlayer() {
+    this->player = new Player();
+}
+
+void Game::initTextures() {
+    this->textures["BUILDING"] = new sf::Texture();
+    this->textures["BUILDING"]->loadFromFile("../Textures/buildings/building_1/building_8.png");
+    this->textures["GRASS"] = new sf::Texture();
+    this->textures["GRASS"]->loadFromFile("../Textures/land/land_1.png");
+    this->textures["TREE"] = new sf::Texture();
+    this->textures["TREE"]->loadFromFile("../Textures/decor/tree_1.png");
+    this->textures["STONE"] = new sf::Texture();
+    this->textures["STONE"]->loadFromFile("../Textures/decor/stones_1.png");
+}
+
+void Game::initMap() {
+    this->nMapWidth = 64;
+    this->nMapHeight = 64;
+    this->sMap += "................................................................";
+    this->sMap += "................................................................";
+    this->sMap += "................................................................";
+    this->sMap += "......#................#######..................................";
+    this->sMap += "......................#.........#.#.............................";
+    this->sMap += "...................####.........#.#.............................";
+    this->sMap += "...........##############.......................................";
+    this->sMap += ".#############################.##################..#####........";
+    this->sMap += ".............................#.#..................#.............";
+    this->sMap += ".............................#.#.................#..............";
+    this->sMap += ".............................#.#................#...............";
+    this->sMap += ".........................#####.#...............#................";
+    this->sMap += ".........................#.....#..............#.................";
+    this->sMap += ".........................#.#####.............#..................";
+    this->sMap += ".........................#..................#...................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###.....###########....................";
+    this->sMap += ".........................###.....###########....................";
+    this->sMap += ".........................###.....###########....................";
+    this->sMap += ".........................###.....###########....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................#########.......###....................";
+    this->sMap += ".........................#########.......###....................";
+    this->sMap += ".........................#########.......###....................";
+    this->sMap += ".........................#########.......###....................";
+    this->sMap += ".........................#########.......###....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###........########....................";
+    this->sMap += ".........................###........########....................";
+    this->sMap += ".........................###........########....................";
+    this->sMap += ".........................###........########....................";
+    this->sMap += ".........................###........########....................";
+    this->sMap += ".........................###........########....................";
+    this->sMap += ".........................###........########....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###########......##....................";
+    this->sMap += ".........................###########......##....................";
+    this->sMap += ".........................###########......##....................";
+    this->sMap += ".........................###########......##....................";
+    this->sMap += ".........................###########......##....................";
+    this->sMap += ".........................###########......##....................";
+    this->sMap += ".........................###########......##....................";
+    this->sMap += ".........................###########......##....................";
+    this->sMap += ".........................###########......##....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###################....................";
+    this->sMap += ".........................###.......#########....................";
+    this->sMap += ".........................###.......#########....................";
+    this->sMap += ".........................###.......#########....................";
+    this->sMap += ".........................###.......#########....................";
+    this->sMap += ".........................###.......#########....................";
+    this->sMap += ".........................###.......#########....................";
+    this->sMap += ".........................###.......#########....................";
+    this->sMap += ".........................###.......#########....................";
+    this->sMap += ".........................###.......#########....................";
+    this->sMap += ".........................###################....................";
 }
 
 void Game::pollEvents() {
@@ -38,12 +122,23 @@ void Game::pollEvents() {
 Game::Game() {
     this->initVariables();
     this->initWindow();
-    this->initBuildings();
+    this->initTextures();
+    this->initPlayer();
+    this->initMap();
 }
 
 Game::~Game() {
     ImGui::SFML::Shutdown();
     delete this->window;
+    delete this->player;
+
+    for (const auto &i : textures) {
+        delete i.second;
+    }
+
+    for (const auto &i : game_objects) {
+        delete i;
+    }
 }
 
 // accessors
@@ -58,10 +153,125 @@ void Game::run() {
 
 void Game::update() {
     this->pollEvents();
+    this->updateInput();
+    this->updateObjects();
+    this->updateMap();
     ImGui::SFML::Update(*this->window, this->deltaClock.restart());
 }
 
+void Game::updateInput() {
+    this->fPlayerVelX = 0.f;
+    this->fPlayerVelY = 0.f;
+
+    // move player
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        this->fPlayerVelY = -1.f;
+    //this->player->move(0.f, -1.f);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        this->fPlayerVelY = 1.f;
+    //this->player->move(0.f, 1.f);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        this->fPlayerVelX = -1.f;
+    //this->player->move(-1.f, 0.f);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        this->fPlayerVelX = 1.f;
+    //this->player->move(1.f, 0.f);
+
+    fPlayerPosX = fPlayerPosX + fPlayerVelX * 0.2f;
+    fPlayerPosY = fPlayerPosY + fPlayerVelY * 0.2f; // TODO: get elapsed time here
+
+//    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+//        this->game_objects.push_back(new Object(this->textures["BUILDING"], sf::Mouse::getPosition(*this->window).x,
+//                                           sf::Mouse::getPosition(*this->window).y, 0.f, 0.f, 1.f));
+//    }
+
+    fCameraPosX = fPlayerPosX;
+    fCameraPosY = fPlayerPosY;
+}
+
+void Game::updateObjects() {
+    for (auto *obj : this->game_objects) {
+        obj->update();
+    }
+}
+
+void Game::updateMap() {
+    // lambda for accessing string as a 2d array
+    auto GetTile = [&](int x, int y) {
+        if (x >= 0 && x < nMapWidth && y >= 0 && y < nMapHeight)
+            return sMap[y * nMapWidth + x];
+        else
+            return ' ';
+    };
+
+    auto SetTile = [&](int x, int y, char c) {
+        if (x >= 0 && x < nMapWidth && y >= 0 && y < nMapHeight)
+            sMap[y * nMapWidth + x] = c;
+    };
+
+    int nTileWidth = 32;
+    int nTileHeight = 32;
+    int nVisibleTilesX = this->window->getSize().x / nTileWidth;
+    int nVisibleTilesY = this->window->getSize().y / nTileHeight;
+
+    // leftmost tile position
+    float fOffsetX = fCameraPosX - (float) nVisibleTilesX / 2.0f;
+    float fOffsetY = fCameraPosY - (float) nVisibleTilesY / 2.0f;
+
+    // clamp camera to game bounds
+    if (fOffsetX < 0) fOffsetX = 0;
+    if (fOffsetY < 0) fOffsetY = 0;
+    if (fOffsetX > nMapWidth - nVisibleTilesX) fOffsetX = nMapWidth - nVisibleTilesX;
+    if (fOffsetY > nMapHeight - nVisibleTilesY) fOffsetY = nMapHeight - nVisibleTilesY;
+
+    sf::RectangleShape rectangle;
+    rectangle.setSize(sf::Vector2f(nTileWidth, nTileHeight));
+    rectangle.setOutlineColor(sf::Color::Black);
+    rectangle.setOutlineThickness(2);
+
+    this->window->clear(sf::Color::White);
+    // draw visible tile map TODO: rozdzielic renderowanie od aktualizowania
+    for (int x = -1; x < nVisibleTilesX + 1; ++x) {
+        for (int y = -1; y < nVisibleTilesY + 1; ++y) {
+            char tileID = GetTile(x + fOffsetX, y + fOffsetY);
+            rectangle.setPosition(x * nTileWidth, y * nTileHeight);
+            switch (tileID) {
+                case '.':
+                    rectangle.setFillColor(sf::Color::Cyan);
+                    this->window->draw(rectangle);
+                    break;
+                case '#':
+                    rectangle.setFillColor(sf::Color::Red);
+                    this->window->draw(rectangle);
+                    break;
+                case 'T':
+                    rectangle.setFillColor(sf::Color::Green);
+                    this->window->draw(rectangle);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    // draw player
+    rectangle.setFillColor(sf::Color::Magenta);
+    rectangle.setPosition((fPlayerPosX - fOffsetX) * nTileWidth, (fPlayerPosY - fOffsetY) * nTileHeight);
+    this->window->draw(rectangle);
+
+}
+
 void Game::render() {
+
+    //this->window->clear(sf::Color::White); TODO:uncomment and remove it from update()
+
+    // render objects here
+    this->player->render(*this->window);
+
+    for (auto *obj : this->game_objects) {
+        obj->render(*this->window);
+    }
+
     ImGui::Begin("Panel misji"); // begin window
     for (size_t idx = 1; auto &[str, state] : this->plans) {
         ImGui::Checkbox((std::to_string(idx) + std::string(": ") + str).c_str(), &state);
@@ -70,16 +280,13 @@ void Game::render() {
     ImGui::Button("Ukoncz misje");
     ImGui::End(); // end window
 
-    this->window->clear();
-    this->window->draw(this->building);
     ImGui::SFML::Render(*this->window);
     this->window->display();
 }
 
-void Game::initBuildings() {
-    this->building.setPosition(10.f, 10.f);
-    this->building.setSize(sf::Vector2f(100.f, 100.f));
-    this->building.setFillColor(sf::Color::Red);
-    this->building.setOutlineColor(sf::Color::Green);
-    this->building.setOutlineThickness(1.f);
-}
+
+
+
+
+
+
--- Game.h
+++ Game.h
@@ -5,31 +5,49 @@
 #ifndef SIMCITY_GAME_H
 #define SIMCITY_GAME_H
 
-#include <imgui.h>
-#include <imgui-SFML.h>
-
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Graphics/RectangleShape.hpp>
-#include <SFML/Graphics/Texture.hpp>
-#include <SFML/Graphics/Sprite.hpp>
+#include <map>
+#include "Player.h"
+#include "Object.h"
 
 /*
  * Class that acts as the game engine.
  * Wrapper for SFML stuff.
  */
 class Game {
-private:
-    // variables
+    // private variables
     sf::RenderWindow *window = nullptr;
+
+    std::map<char, std::string> my_map = {
+            {'#', "STONE"},
+            {'T', "TREE"},
+            {'.', "GRASS"}
+    };
+
+    std::map<std::string, sf::Texture *> textures;
+
     sf::Event event{};
     std::string title;
     sf::Clock deltaClock;
-    float scale_factor = 0.0f;
+    float gui_scale_factor = 0.0f;
 
     // game objects
-    sf::RectangleShape building;
+    Player *player;
+    std::vector<Object *> game_objects;
+    // map object
+    std::vector<sf::Texture *> vMap;
+    std::string sMap;
+
+    int nMapWidth;
+    int nMapHeight;
+
+    float fPlayerPosX = 0.0f;
+    float fPlayerPosY = 0.0f;
+
+    float fPlayerVelX = 0.0f;
+    float fPlayerVelY = 0.0f;
+
+    float fCameraPosX = 0.0f;
+    float fCameraPosY = 0.0f;
 
     std::vector<std::tuple<std::string, bool>> plans = {{"Jakies",  false},
                                                         {"Bzdety",  false},
@@ -40,10 +58,19 @@ private:
 
     void initWindow();
 
-    void initBuildings();
+    void initTextures();
+
+    void initPlayer();
+
+    void initMap();
 
     void pollEvents();
 
+    void updateInput();
+
+    void updateObjects();
+
+    void updateMap(); // TODO: this updates camera (change name)
     void update();
 
     void render();
